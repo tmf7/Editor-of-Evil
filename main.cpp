cddc0bf6e4fd390596bdb510869f14caf985c9ea
tmf7 <tmf7@njit.edu>
2016-12-31 21:38:41 -0500

    initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+//**********************************************
+//*
+//*Copyright Thomas Freehill December 31 2016
+//*This editor is intended to output resources
+//*for use by the EngineOfEvil and Evil game
+//**********************************************
+
+#include <Windows.h>
+#include "SDL.h"
+#include "SDL_ttf.h"
+#include "SDL_image.h"
+#include <cstring>			// memset
+#include <ios>
+#include <fstream>
+#include <random>
+#include <limits>
+//#include <stdio.h>
+
+
+int __stdcall WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow) {
+
+
+	return 0;
+}
